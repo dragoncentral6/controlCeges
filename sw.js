05d63073d09ee7df0e69a9196ce9c32d3a9d9fb6
dragoncentral6 <dragon6.central@gmail.com>
2026-07-02 05:45:23 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = 'v1_cache_miapp';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/script.js',
+  '/images/icon-192.png'
+];
+
+// Instalar el Service Worker y almacenar recursos en caché
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+      .then(() => self.skipWaiting())
+  );
+});
+
+// Activar el Service Worker y limpiar cachés antiguas
+self.addEventListener('activate', e => {
+  const cacheWhitelist = [CACHE_NAME];
+  e.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+// Responder con los recursos en caché o buscarlos en la red
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request)
+      .then(res => {
+        if (res) {
+          return res;
+        }
+        return fetch(e.request);
+      })
+  );
+});
