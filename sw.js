e0b7f732d07e0ff295a301e5bfe23c06e99403ea
dragoncentral6 <dragon6.central@gmail.com>
2026-07-02 05:53:53 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ const urlsToCache = [
   '/index.html',
   '/estilos.css',
   '/app.js',
-  '/images/iconMedio.png'
+  '/images/iconMedia.png'
 ];
 
 // Instalar el Service Worker y almacenar recursos en caché
